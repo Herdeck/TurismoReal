98e929095c9642d97e895a7eb418b52ea3af28e2
FranciscoQuiroz <her.deck1@gmail.com>
2021-12-04 21:39:05 -0300

    Creación de Reserva crud

---
+++ TurismoReal.Negocio/Multa.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurismoReal.DALC;
+
+namespace TurismoReal.Negocio
+{
+    public class Multa
+    {
+
+        public decimal IdMulta { get; set; }
+        public string Detalle { get; set; }
+        public decimal ValorMulta { get; set; }
+
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+
+        public List<Multa> ReadAll()
+        {
+            return this.db.MULTAS.Select(i => new Multa()
+            {
+                IdMulta = i.ID_MULTA,
+                Detalle = i.DETALLE,
+                ValorMulta = i.VALOR_MULTA
+
+            }).ToList();
+
+
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_MULTAS(this.IdMulta, this.Detalle, this.ValorMulta);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public Multa Find(int IdMulta)
+        {
+            return this.db.MULTAS.Select(p => new Multa()
+            {
+
+
+
+                IdMulta = p.ID_MULTA,
+                Detalle = p.DETALLE,
+                ValorMulta = p.VALOR_MULTA
+
+            }).Where(p => p.IdMulta == IdMulta).FirstOrDefault();
+
+        }
+        public bool Update()
+        {
+            try
+            {
+                db.SP_UPDATE_MULTAS(this.IdMulta, this.Detalle, this.ValorMulta);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+          
+
+        }
+        public bool Delete(int IdMulta)
+        {
+            try
+            {
+                db.SP_DELETE_MULTAS(IdMulta);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ TurismoReal/Controllers/MultaController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+namespace TurismoReal.Controllers
+{
+    public class MultaController : Controller
+    {
+        // GET: Multa
+        public ActionResult Index()
+        {
+            ViewBag.multa = new Multa().ReadAll();
+
+            return View();
+        }
+
+        // GET: Multa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Multa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Multa/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "IdMulta,Detalle,ValorMulta")] Multa multa)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                multa.Save();
+                TempData["mensaje"] = "Item guardado corectamente";
+                return RedirectToAction("IndexMulta");
+            
+                return RedirectToAction("IndexMulta");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: Multa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Multa i = new Multa().Find(id);
+
+            if (i == null)
+            {
+                TempData["mensaje"] = "el item no existe";
+                return RedirectToAction("IndexMulta");
+            }
+            ViewBag.multa = new Multa().ReadAll();
+         
+
+            return View(i);
+        }
+
+        // POST: Multa/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include ="IdMulta,Detalle,ValorMulta")] Multa multa)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                multa.Update();
+                TempData["mensaje"] = "editado correctamente";
+                return RedirectToAction("IndexMulta");
+            }
+            catch
+            {
+                return View(multa);
+            }
+        }
+
+        // GET: Multa/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            if (new Multa().Find(id) == null)
+            {
+                TempData["mensaje"] = "no existe el producto";
+                return RedirectToAction("IndexMulta");
+            }
+
+            if (new Multa().Delete(id))
+            {
+                TempData["mensaje"] = "Eliminado correctamente";
+                return RedirectToAction("IndexMulta");
+            }
+            TempData["mensaje"] = "no se pudo eliminar";
+            return RedirectToAction("IndexMulta");
+        }
+    
+
+        // POST: Multa/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("IndexMulta");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
