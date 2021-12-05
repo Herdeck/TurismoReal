7746f13ba883b597136e0ee72311995528888737
FranciscoQuiroz <her.deck1@gmail.com>
2021-12-05 20:57:31 -0300

    Creación listado FotosDepto

---
+++ TurismoReal.Negocio/FotoDepto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using TurismoReal.DALC;
+
+namespace TurismoReal.Negocio
+{
+    public class FotoDepto
+    {
+        [Required,Range(0,10000)]
+        public decimal Id { get; set; }
+        [Required]
+        public byte[] Foto { get; set; }
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+        public List<FotoDepto> ReadAll()
+        {
+            return this.db.FOTO_DEPTO.Select(f => new FotoDepto()
+            {
+                Id = f.ID_DEPTO,
+                Foto = f.FOTO
+            }).ToList();
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_FOTODEPTO(this.Id, this.Foto);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public FotoDepto Find(int id)
+        {
+            return this.db.FOTO_DEPTO.Select(f => new FotoDepto()
+            {
+                Id = f.ID_DEPTO,
+                Foto = f.FOTO
+            }).Where(f => f.Id == id).FirstOrDefault();
+        }
+
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_CREATE_FOTODEPTO(this.Id, this.Foto);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_FOTODEPTO(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ TurismoReal/Controllers/FotoDeptoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+
+namespace TurismoReal.Controllers
+{
+    public class FotoDeptoController : Controller
+    {
+        // GET: FotoDepto
+        public ActionResult Index()
+        {
+            ViewBag.fotodepto = new FotoDepto().ReadAll();
+            return View();
+        }
+
+        // GET: FotoDepto/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FotoDepto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FotoDepto/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="Id,Foto")]FotoDepto foto, HttpPostedFileBase file)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.fotodepto = new FotoDepto().ReadAll();
+                    return View(foto);
+                }
+                foto.Save();
+                TempData["mensaje"] = "Guardado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(foto);
+            }
+        }
+
+        // GET: FotoDepto/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FotoDepto/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FotoDepto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FotoDepto/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
