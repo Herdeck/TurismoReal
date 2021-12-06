2851419177c7dcd02c2220984f30a77cf87b1f8f
Alexander Torres <al.torress@duocuc.cl>
2021-12-06 04:20:22 -0300

    Crud de CheckIn

---
+++ TurismoReal.Negocio/CheckIn.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurismoReal.DALC;
+
+namespace TurismoReal.Negocio
+{
+    public class CheckIn
+    {
+        
+
+        [Required, Range(0, 10000)]
+        public decimal IdCheckIn { get; set; }
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public decimal IdDepto { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public decimal IdPago { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+
+        public decimal IdServicio { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+
+        public byte[] DocumentoAceptacion { get; set; }
+
+
+
+
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+        public List<CheckIn> ReadAll()
+        {
+            return this.db.CHECK_IN.Select(i => new CheckIn()
+            {
+                IdCheckIn = i.ID_CHECKIN,
+                IdDepto = i.ID_DEPTO,
+                IdPago = i.ID_PAGO,
+                IdServicio = i.ID_SERVICIO,
+                DocumentoAceptacion = i.DOCUMENTO_ACEPTACIÓN
+            }).ToList();
+        }
+
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_CHECKIN(this.IdCheckIn, this.IdDepto, this.IdPago, this.IdServicio, this.DocumentoAceptacion);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public CheckIn Find(int id)
+        {
+            return this.db.CHECK_IN.Select(i => new CheckIn()
+        
+            {
+                IdCheckIn = i.ID_CHECKIN,
+                IdDepto = i.ID_DEPTO,
+                IdPago = i.ID_PAGO,
+                IdServicio = i.ID_SERVICIO,
+                DocumentoAceptacion = i.DOCUMENTO_ACEPTACIÓN
+
+            }).Where(i => i.IdCheckIn == id).FirstOrDefault();
+        }
+
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_UPDATE_CHECKIN(this.IdCheckIn, this.IdDepto, this.IdPago, this.IdServicio, this.DocumentoAceptacion);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_CHECKIN(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ TurismoReal/Controllers/CheckInController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+
+namespace TurismoReal.Controllers
+{
+    [Authorize]
+    public class CheckInController : Controller
+    {
+        // GET: CheckIn
+        public ActionResult Index()
+        {
+            ViewBag.checkin = new CheckIn().ReadAll();
+            return View();
+        }
+
+        // GET: CheckIn/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CheckIn/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckIn/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "IdCheckIn,IdDepto,IdPago,IdServicio,DocumentacionAceptacion")]CheckIn checkin)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                checkin.Save();
+                TempData["mensaje"] = "Item guardado corectamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: CheckIn/Edit/5
+        public ActionResult Edit(int id)
+        {
+            CheckIn i = new CheckIn().Find(id);
+
+            if (i == null)
+            {
+                TempData["mensaje"] = "el item no existe";
+                return RedirectToAction("Index");
+            }
+            ViewBag.checkin = new CheckIn().ReadAll();
+
+            return View(i);
+        }
+
+        // POST: CheckIn/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "IdCheckIn,IdDepto,IdPago,IdServicio,DocumentacionAceptacion")] CheckIn checkin)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                checkin.Update();
+                TempData["mensaje"] = "editado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(checkin);
+            }
+        }
+
+        // GET: CheckIn/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckIn/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
