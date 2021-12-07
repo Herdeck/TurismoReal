e15f40ea7cae40a68dee1bff3c20fb3598dee5e3
FranciscoQuiroz <her.deck1@gmail.com>
2021-12-06 21:47:48 -0300

    Creación curd ServiciosExtra

---
+++ TurismoReal.Negocio/ServiciosExtra.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurismoReal.DALC;
+using System.ComponentModel.DataAnnotations;
+
+namespace TurismoReal.Negocio
+{
+    public class ServiciosExtra
+    {
+        [Required, Range(0, 10000)]
+        public decimal IdServicio { get; set; }
+        [Required, Range(0, 10000)]
+        public decimal IdReserva { get; set; }
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public string NombreServicio { get; set; }
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public string Descripcion { get; set; }
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+        public List<ServiciosExtra> ReadAll()
+        {
+            return this.db.SERVICIOS_EXTRA.Select(s => new ServiciosExtra()
+            {
+                IdServicio = s.ID_SERVICIO,
+                IdReserva = s.ID_RESERVA,
+                NombreServicio = s.NOMBRE_SERVICIO,
+                Descripcion = s.DESCRIPCION
+            }).ToList();
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_SERVICIOSEXTRA(this.IdServicio,this.IdReserva,this.NombreServicio,this.Descripcion);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public ServiciosExtra Find(int id)
+        {
+            return this.db.SERVICIOS_EXTRA.Select(s => new ServiciosExtra()
+            {
+                IdServicio = s.ID_SERVICIO,
+                IdReserva = s.ID_RESERVA,
+                NombreServicio = s.NOMBRE_SERVICIO,
+                Descripcion = s.DESCRIPCION
+            }).Where(s => s.IdServicio == id).FirstOrDefault();
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_CREATE_SERVICIOSEXTRA(this.IdServicio, this.IdReserva, this.NombreServicio, this.Descripcion);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_SERVICIOSEXTRA(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ TurismoReal/Controllers/ServiciosExtraController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+
+namespace TurismoReal.Controllers
+{
+    public class ServiciosExtraController : Controller
+    {
+        // GET: ServiciosExtra
+        public ActionResult Index()
+        {
+            ViewBag.serviciosextra = new ServiciosExtra().ReadAll();
+            return View();
+        }
+
+        // GET: ServiciosExtra/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ServiciosExtra/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiciosExtra/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="IdServicio,IdReserva,NombreServicio,Descripcion")]ServiciosExtra servicios)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.serviciosextra = new ServiciosExtra().ReadAll();
+                    return View(servicios);
+                }
+                servicios.Save();
+                TempData["mensaje"] = "Guardado Correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(servicios);
+            }
+        }
+
+        // GET: ServiciosExtra/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ServiciosExtra s = new ServiciosExtra().Find(id);
+            if(s == null)
+            {
+                TempData["mensaje"] = "servicio extra no encontrado";
+                return RedirectToAction("Index");
+            }
+            ViewBag.serviciosextra = new ServiciosExtra().ReadAll();
+            return View(s);
+        }
+
+        // POST: ServiciosExtra/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "IdServicio,IdReserva,NombreServicio,Descripcion")]ServiciosExtra servicios)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.serviciosextra = new ServiciosExtra().ReadAll();
+                    return View(servicios);
+                }
+                servicios.Update();
+                TempData["mensaje"] = "Editado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(servicios);
+            }
+        }
+
+        // GET: ServiciosExtra/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if(new ServiciosExtra().Find(id) == null)
+            {
+                TempData["mensaje"] = "no existe el servicio extra";
+                return RedirectToAction("Index");
+            }
+            if(new ServiciosExtra().Delete(id))
+            {
+                TempData["mensaje"] = "Eliminado correctamente";
+                return RedirectToAction("Index");
+            }
+            TempData["mensaje"] = "no se pudo eliminar";
+            return RedirectToAction("Index");
+        }
+
+        // POST: ServiciosExtra/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
