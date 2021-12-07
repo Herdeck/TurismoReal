79cbe54009a54eb4fe9eaa330935ea821f62971e
Alexander Torres <al.torress@duocuc.cl>
2021-12-07 01:09:50 -0300

    Crud CheckOut

---
+++ TurismoReal.Negocio/CheckOut.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurismoReal.DALC;
+
+namespace TurismoReal.Negocio
+{
+    public class CheckOut
+    {
+
+
+        [Required, Range(0, 10000)]
+        public decimal IdCheckOut { get; set; }
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public decimal IdMulta{ get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+        public decimal IdDepto { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+
+        public decimal IdReserva { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "El {0} debe contener minimo {1} caracteres"), MaxLength(50)]
+
+        public byte[] DocumentoEntrega{ get; set; }
+
+
+
+
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+        public List<CheckOut> ReadAll()
+        {
+            return this.db.CHECK_OUT.Select(i => new CheckOut()
+            {
+               IdCheckOut = i.ID_CHECKOUT,
+               IdMulta    = (decimal)i.ID_MULTA,
+               IdDepto    = i.ID_DEPTO,
+               IdReserva  = i.ID_RESERVA,
+               DocumentoEntrega = i.DOCUMENTO_ENTREGA
+
+
+
+              
+            }).ToList();
+        }
+
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_CHECKOUT(this.IdCheckOut,this.IdMulta,this.IdDepto,this.IdReserva,this.DocumentoEntrega);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public CheckOut Find(int id)
+        {
+            return this.db.CHECK_OUT.Select(i => new CheckOut()
+
+            {
+               IdCheckOut = i.ID_CHECKOUT,
+               IdMulta    = (decimal)i.ID_MULTA,
+               IdDepto    = i.ID_DEPTO,
+               IdReserva  = i.ID_RESERVA,
+               DocumentoEntrega = i.DOCUMENTO_ENTREGA
+
+
+            }).Where(i => i.IdCheckOut == id).FirstOrDefault();
+        }
+
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_UPDATE_CHECKOUT(this.IdCheckOut,this.IdMulta,this.IdDepto,this.IdReserva,this.DocumentoEntrega);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_CHECKOUT(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ TurismoReal/Controllers/CheckoOutController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+
+namespace TurismoReal.Controllers
+{
+    public class CheckoOutController : Controller
+    {
+        // GET: CheckoOut
+        public ActionResult Index()
+        {
+
+            ViewBag.checkin = new CheckOut().ReadAll();
+            return View();
+        }
+
+        // GET: CheckoOut/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CheckoOut/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckoOut/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "IdCheckOut,IdMulta,IdDepto,IdReserva,DocumentoEntrega")] CheckOut checkout)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                checkout.Save();
+                TempData["mensaje"] = "Item guardado corectamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: CheckoOut/Edit/5
+        public ActionResult Edit(int id)
+        {
+            {
+                CheckIn i = new CheckIn().Find(id);
+
+                if (i == null)
+                {
+                    TempData["mensaje"] = "el item no existe";
+                    return RedirectToAction("Index");
+                }
+                ViewBag.checkin = new CheckIn().ReadAll();
+
+                return View(i);
+            }
+        }
+
+        // POST: CheckoOut/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "IdCheckOut,IdMulta,IdDepto,IdReserva,DocumentoEntrega")] CheckOut checkout)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                checkout.Update();
+                TempData["mensaje"] = "editado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(checkout);
+            }
+        }
+        
+
+        // GET: CheckoOut/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckoOut/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
