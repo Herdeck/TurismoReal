64d0f549c13a984526ef11e345cc258c05bf286f
FranciscoQuiroz <her.deck1@gmail.com>
2021-12-06 23:26:30 -0300

    Actualización crud pago reserva

---
+++ TurismoReal.Negocio/PagoReserva.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurismoReal.DALC;
+using System.ComponentModel.DataAnnotations;
+
+namespace TurismoReal.Negocio
+{
+    public class PagoReserva
+    {
+        [Required, Range(0, 10000)]
+        public decimal IdPago { get; set; }
+        [Required, Range(0, 10000)]
+        public decimal IdReserva { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-mm-yy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaPago { get; set; }
+        [Required, Range(1, 1000000)]
+        public decimal Monto { get; set; }
+
+        TurismoRealEntities db = new TurismoRealEntities();
+
+        public List<PagoReserva> ReadAll()
+        {
+            return this.db.PAGO_RESERVA.Select(p => new PagoReserva() { 
+                IdPago = p.ID_PAGO,
+                IdReserva = p.ID_RESERVA,
+                FechaPago = p.FECHA_PAGO,
+                Monto = p.MONTO
+            }).ToList();
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                db.SP_CREATE_PAGORESERVA(this.IdPago,this.IdReserva,this.FechaPago,this.Monto);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public PagoReserva Find(int id)
+        {
+            return this.db.PAGO_RESERVA.Select(p => new PagoReserva()
+            {
+                IdPago = p.ID_PAGO,
+                IdReserva = p.ID_RESERVA,
+                FechaPago = p.FECHA_PAGO,
+                Monto = p.MONTO
+            }).Where(p => p.IdPago == id).FirstOrDefault();
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_UPDATE_PAGORESERVA(this.IdPago, this.IdReserva, this.FechaPago, this.Monto);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_PAGORESERVA(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ TurismoReal/Controllers/PagoReservaController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurismoReal.Negocio;
+
+namespace TurismoReal.Controllers
+{
+    public class PagoReservaController : Controller
+    {
+        // GET: PagoReserva
+        public ActionResult Index()
+        {
+            ViewBag.pagoreserva = new PagoReserva().ReadAll();
+            return View();
+        }
+
+        // GET: PagoReserva/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PagoReserva/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PagoReserva/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="IdPago,IdReserva,FechaPago,Monto")]PagoReserva pago)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.pagoreserva = new PagoReserva().ReadAll();
+                    return View(pago);
+                }
+                pago.Save();
+                TempData["mensaje"] = "pago reserva creado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(pago);
+            }
+        }
+
+        // GET: PagoReserva/Edit/5
+        public ActionResult Edit(int id)
+        {
+            PagoReserva p = new PagoReserva().Find(id);
+            if(p == null)
+            {
+                TempData["mensaje"] ="pago reserva no encontrado";
+                return RedirectToAction("Index");
+            }
+            ViewBag.pagoreserva = new PagoReserva().ReadAll();
+            return View(p);
+        }
+
+        // POST: PagoReserva/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include ="IdPago,IdReserva,FechaPago,Monto")]PagoReserva pago)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if(!ModelState.IsValid)
+                {
+                    ViewBag.pagoreserva = new PagoReserva().ReadAll();
+                    return View(pago);
+                }
+                pago.Update();
+                TempData["mensaje"] = "pago reserva editado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(pago);
+            }
+        }
+
+        // GET: PagoReserva/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if(new PagoReserva().Find(id) == null)
+            {
+                TempData["mensaje"] ="no se encontro el pago";
+                return RedirectToAction("Index");
+            }
+            if(new PagoReserva().Delete(id))
+            {
+                TempData["mensaje"] ="Eliminado correctamente";
+                return RedirectToAction("Index");
+            }
+            TempData["mensaje"] ="no se pudo eliminar";
+            return RedirectToAction("Index");
+        }
+
+        // POST: PagoReserva/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
